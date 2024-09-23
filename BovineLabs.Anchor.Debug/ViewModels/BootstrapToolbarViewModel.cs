712c54e850cfc388a3060e74fcb1832d1ab8ecf0
Timothy Raines <tim@ninjatertle.com>
2024-09-23 10:55:37 +1000

    Fixed Core Extension dependency.

--- BovineLabs.Anchor.Debug/ViewModels/BootstrapToolbarViewModel.cs
+++ BovineLabs.Anchor.Debug/ViewModels/BootstrapToolbarViewModel.cs
@@ -2,7 +2,7 @@
 //     Copyright (c) BovineLabs. All rights reserved.
 // </copyright>
 
-#if BL_CORE
+#if BL_CORE && BL_CORE_EXTENSIONS
 namespace BovineLabs.Anchor.Debug.ViewModels
 {
     using BovineLabs.Core;
