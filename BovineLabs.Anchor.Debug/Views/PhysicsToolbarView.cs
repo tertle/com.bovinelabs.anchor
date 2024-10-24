04bb92fe2f60b91b94590947cab40199ca347927
Timothy Raines <tim@ninjatertle.com>
2024-10-24 15:18:30 +1100

    Fixed up PhysicsToolbarView

--- BovineLabs.Anchor.Debug/Views/PhysicsToolbarView.cs
+++ BovineLabs.Anchor.Debug/Views/PhysicsToolbarView.cs
@@ -7,11 +7,14 @@ namespace BovineLabs.Anchor.Debug.Views
 {
     using BovineLabs.Anchor;
     using BovineLabs.Anchor.Debug.ViewModels;
+    using JetBrains.Annotations;
     using Unity.Properties;
+    using UnityEngine.Scripting;
     using UnityEngine.UIElements;
     using Toggle = Unity.AppUI.UI.Toggle;
 
     [Transient]
+    [UsedImplicitly]
     public class PhysicsToolbarView : VisualElement, IView<PhysicsToolbarViewModel>
     {
         public const string UssClassName = "bl-physics-tab";
@@ -28,56 +31,29 @@ namespace BovineLabs.Anchor.Debug.Views
             this.Add(right);
 
             var colliders = new Toggle { label = "Colliders", dataSource = this.ViewModel };
-            colliders.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawColliderEdges)),
-            });
+            colliders.RegisterValueChangedCallback(evt => this.ViewModel.DrawColliderEdges = evt.newValue);
             left.Add(colliders);
 
             var aabbs = new Toggle { label = "AABBs", dataSource = this.ViewModel };
-            aabbs.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawColliderAabbs)),
-            });
-
+            aabbs.RegisterValueChangedCallback(evt => this.ViewModel.DrawColliderAabbs = evt.newValue);
             left.Add(aabbs);
 
 #if BL_DRAW
             var terrain = new Toggle { label = "Terrain", dataSource = this.ViewModel };
-
-            terrain.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawTerrainColliderEdges)),
-            });
-
+            terrain.RegisterValueChangedCallback(evt => this.ViewModel.DrawTerrainColliderEdges = evt.newValue);
             left.Add(terrain);
 
             var mesh = new Toggle { label = "Mesh", dataSource = this.ViewModel };
-            mesh.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawMeshColliderEdges)),
-            });
+            mesh.RegisterValueChangedCallback(evt => this.ViewModel.DrawMeshColliderEdges = evt.newValue);
             right.Add(mesh);
 #endif
 
             var collisions = new Toggle { label = "Collisions", dataSource = this.ViewModel };
-            collisions.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawCollisionEvents)),
-            });
+            collisions.RegisterValueChangedCallback(evt => this.ViewModel.DrawCollisionEvents = evt.newValue);
             right.Add(collisions);
 
             var triggers = new Toggle { label = "Triggers", dataSource = this.ViewModel };
-            triggers.SetBinding(nameof(Toggle.value), new DataBinding
-            {
-                bindingMode = BindingMode.ToSource,
-                dataSource = new PropertyPath(nameof(PhysicsToolbarViewModel.DrawTriggerEvents)),
-            });
+            triggers.RegisterValueChangedCallback(evt => this.ViewModel.DrawTriggerEvents = evt.newValue);
             right.Add(triggers);
         }
 
