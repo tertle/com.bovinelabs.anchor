cd9f2497e21933c9d332aaf59910ee718cfc6d24
Timothy Raines <tim@ninjatertle.com>
2025-02-05 08:38:01 +1100

    Added UISystemGroup

---
+++ BovineLabs.Anchor/UISystemGroup.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="UISystemGroup.cs" company="BovineLabs">
+//     Copyright (c) BovineLabs. All rights reserved.
+// </copyright>
+
+#if UNITY_ENTITIES
+namespace BovineLabs.Anchor
+{
+    using Unity.Entities;
+
+    [WorldSystemFilter(WorldSystemFilterFlags.Presentation, WorldSystemFilterFlags.Presentation)]
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public partial class UISystemGroup : ComponentSystemGroup
+    {
+    }
+}
+#endif
